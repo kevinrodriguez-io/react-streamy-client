4a7e07a8b84e1b21bc960788ccd40407e2575a90
Kevin Rodríguez <_@kevinrodriguez.io>
2019-02-26 17:03:47 -0600

    Update StreamCreate.js

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -25,6 +25,18 @@ class StreamCreate extends Component {
     );
   };
 }
+
+const validate = (formValues) => {
+  const errors = {}
+  if (!formValues.title) {
+    errors.title = 'You must provide a title';
+  }
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+  return errors;
+};
+
 export default reduxForm({
   form: 'streamCreate'
 })(StreamCreate);
